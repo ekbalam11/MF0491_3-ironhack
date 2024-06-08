0ac45e8eec2f31c89d04122be45a0d606a0e6358
ekbalam11 <ekbalam11@gmail.com>
2024-06-08 11:38:43 +0200

    uf1842 2-DOM  done

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,12 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+
+const btn = document.getElementById('btn-send');
+const input = document.querySelector('input');
+
+btn.addEventListener('click', function(){
+    console.log(input.value);
+    document.getElementById('last-message').textContent = input.value;
+    input.value = ""
+});
