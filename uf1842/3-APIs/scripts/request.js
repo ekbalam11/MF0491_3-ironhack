a124b990a3d81a017eaf69e265caefa1bdf0147f
ekbalam11 <ekbalam11@gmail.com>
2024-06-08 13:08:03 +0200

    uf1843 excercise done

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,5 @@
 async function getPuzzle(wordCount) {
-    const response = await fetch ("https://puzzle.mead.io/puzzle?wordCount=3");
+    const response = await fetch ("https://puzzle.mead.io/puzzle");
     let data = await response.json();
     console.dir(data);
     let hiddenSentence = data.puzzle;
